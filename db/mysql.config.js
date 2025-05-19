a3592253ebd9582fa06f3079f070402571bd5399
ChYote <yote6917@gmail.com>
2025-05-19 09:06:19 +0700

    last commit

--- db/mysql.config.js
+++ db/mysql.config.js
@@ -4,13 +4,14 @@ const { DataSource } = require('typeorm');
 const mysqlDS = new DataSource({
     type: 'mysql',
     host: "127.0.0.1",
+    // host: "localhost",
     port: 3306,
     username: "root",
-    password: "",
-    database: "GuideBook",
+    password: "newpassword",
+    database: "guidebook",
     entities: ["models/*.{.ts,js}"],
     logging: true,
-    synchronize: false
+    synchronize: true
 
 })
 
